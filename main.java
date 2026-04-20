ad20b67187be90abe6aff58d6bf600fbe89d9622
William <148827581+DaNoobMan@users.noreply.github.com>
2026-04-20 15:47:10 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,7 @@
-
+public class main {
+    public static void main(String[] args) {
+        double lW;
+        double uW;
+        Integer[] startingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    }
+}
